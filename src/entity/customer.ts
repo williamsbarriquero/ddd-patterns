6e8823549fedfb2bc247afb0e178aabeb4b79e4d
williamsbarriquero <williamsgomess@outlook.com>
2023-10-05 15:11:17 -0300

    Regra de negócio

---
+++ src/entity/customer.ts
@@ -0,0 +1,34 @@
+class Customer {
+
+  _id: string;
+  _name: string;
+  _address: string;
+  _active: boolean = true
+
+  constructor(id: string, name: string, address: string) {
+    this._id = id;
+    this._name = name;
+    this._address = address;
+  }
+  
+  activate() {
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+
+  get id() : string {
+    return this._id;
+  }
+
+  get name() : string {
+    return this._name;
+  }
+
+  get address() : string {
+    return this._address;
+  }
+  
+}
