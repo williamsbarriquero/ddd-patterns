d72d9a0daded86b313d9a95b5209b4aca6904237
williamsbarriquero <williamsgomess@outlook.com>
2023-10-07 12:25:00 -0300

    Princípio da autovalidação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,34 +1,38 @@
 class Customer {
 
   _id: string;
-  _name: string;
-  _address: string;
+  _name: string = "";
+  _address: string = "";
   _active: boolean = true
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
   }
   
   activate() {
+    if(this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
 
   deactivate() {
     this._active = false;
-  }
-
-  get id() : string {
-    return this._id;
-  }
-
-  get name() : string {
-    return this._name;
-  }
-
-  get address() : string {
-    return this._address;
   }
   
 }
