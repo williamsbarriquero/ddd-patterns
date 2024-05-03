56fe3d4b5aa22d7d2e79dff236bef6226a0e9521
Williams Barriquero <williamsgomess@outlook.com>
2024-05-03 08:36:55 -0300

    Entidade
    Princípio da autovalidação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,38 +1,39 @@
 class Customer {
 
-  _id: string;
-  _name: string = "";
-  _address: string = "";
-  _active: boolean = true
+    _id: string;
+    _name: string = "";
+    _address: string = "";
+    _active: boolean = false;
 
-  constructor(id: string, name: string) {
-    this._id = id;
-    this._name = name;
-    this.validate();
-  }
-
-  validate() {
-    if (this._name.length === 0) {
-      throw new Error("Name is required");
+    constructor(id: string, name: string) {
+        this._id = id;
+        this._name = name;
+        this.validate();
     }
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
+
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
     }
-  }
 
-  changeName(name: string) {
-    this._name = name;
-  }
-
-  activate() {
-    if(this._address.length === 0) {
-      throw new Error("Address is mandatory to activate a customer");
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
     }
-    this._active = true;
-  }
 
-  deactivate() {
-    this._active = false;
-  }
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        this._active = true;
+    }
 
+    deactivate() {
+        this._active = false;
+    }
 }
