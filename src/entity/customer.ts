79d9940e6cc90d4e2028057363d18185ae961697
Williams Barriquero <williamsgomess@outlook.com>
2024-05-03 17:42:07 -0300

    Value Objects

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,8 +1,10 @@
+import Address from "./address";
+
 class Customer {
 
     _id: string;
     _name: string = "";
-    _address: string = "";
+    _address: Address;
     _active: boolean = false;
 
     constructor(id: string, name: string) {
@@ -27,7 +29,7 @@ class Customer {
     }
 
     activate() {
-        if (this._address.length === 0) {
+        if (this._address === undefined) {
             throw new Error("Address is mandatory to activate a customer");
         }
         this._active = true;
@@ -35,5 +37,9 @@ class Customer {
 
     deactivate() {
         this._active = false;
+    }
+
+    set Address(address: Address) {
+        this._address = address;
     }
 }
