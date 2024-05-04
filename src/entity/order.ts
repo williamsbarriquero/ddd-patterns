64c820f63a7bc452e68dcaf5566ec0df8d84e837
Williams Barriquero <williamsgomess@outlook.com>
2024-05-04 00:37:36 -0300

    Avancando com testes
    Preparando ambiente com testes

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -4,10 +4,16 @@ export default class Order {
     _id: string;
     _customerId: string;
     _items: OrderItem[] = [];
+    _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
+        this._total = this.total()
+    }
+
+    total() {
+        return this._items.reduce((acc, item) => acc + item.price, 0);
     }
 }
