6028c45cbc92fcce3adf8f2b1d83c41d93bf65fa
Williams Barriquero <williamsgomess@outlook.com>
2024-05-04 09:42:01 -0300

    Avancando com testes
    Criando testes de Order

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,16 +1,30 @@
 import OrderItem from "./order_item";
 
 export default class Order {
-    _id: string;
-    _customerId: string;
-    _items: OrderItem[] = [];
-    _total: number;
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[] = [];
+    private _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
         this._total = this.total()
+
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+        if (this._customerId.length === 0) {
+            throw new Error("customerId is required");
+        }
+        if (this._items.length === 0) {
+            throw new Error("Items are required");
+        }
     }
 
     total() {
