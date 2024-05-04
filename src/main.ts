41209b5f4fd6f471a66e64ff59236d045d4f974a
Williams Barriquero <williamsgomess@outlook.com>
2024-05-03 23:55:13 -0300

    Aggregate
    Agregados na prática

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import Customer from "./entity/customer";
+import Address from "./entity/address";
+import OrderItem from "./entity/order_item";
+import Order from "./entity/order";
+
+let customer = new Customer("123", "Williams Barriquero");
+const address = new Address("Rua Um", 2, "54780-455", "Camaragibe");
+customer.Address = address;
+customer.activate();
+
+const item1 = new OrderItem("1", "Item 1", 10);
+const item2 = new OrderItem("2", "Item 2", 15);
+
+new Order("1", "123", [item1, item2]);
