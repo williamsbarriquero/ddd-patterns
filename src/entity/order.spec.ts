5e60c797aadf0886e304e27e4dd580753b1add03
Williams Barriquero <williamsgomess@outlook.com>
2024-05-04 22:56:34 -0300

    Avancando com testes
    Relacionando Item com Product

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -22,15 +22,26 @@ describe('Order unit tests', () => {
   });
 
   it('should calculate total', () => {
-    const item = new OrderItem('i1', 'Item 1', 100);
+    const item = new OrderItem(
+      'i1', 'Item 1', 100, 'p1', 2);
     const order = new Order('o1', 'c1', [item]);
-    const item2 = new OrderItem('i2', 'Item 2', 200);
+    const item2 = new OrderItem(
+      'i2', 'Item 2', 200, 'p2', 2);
 
-    let total = order.total;
-    expect(total).toBe(100);
+    let total = order.total();
+    expect(total).toBe(200);
 
     const order2 = new Order('o2', 'c1', [item, item2]);
-    total = order2.total;
-    expect(total).toBe(300);
+    total = order2.total();
+    expect(total).toBe(600);
+  });
+
+  it('should throw error if the item quantity is less or equal zero', () => {
+
+    expect(() => {
+      const item = new OrderItem(
+        'i1', 'Item 1', 100, 'p1', 0);
+      new Order('o1', 'c1', [item]);
+    }).toThrowError('Quantity must be greater than 0');
   });
 });
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -4,13 +4,11 @@ export default class Order {
   private _id: string;
   private _customerId: string;
   private _items: OrderItem[] = [];
-  private _total: number;
 
   constructor(id: string, customerId: string, items: OrderItem[]) {
     this._id = id;
     this._customerId = customerId;
     this._items = items;
-    this._total = this.total;
 
     this.validate();
   }
@@ -25,13 +23,14 @@ export default class Order {
     if (this._items.length === 0) {
       throw new Error('Items are required');
     }
+    if (this._items.some(item => item.quantity <= 0)) {
+      throw new Error('Quantity must be greater than 0');
+    }
   }
 
-  get total(): number {
-    this._total = this._items.reduce(
-      (acc, item) => acc + item.price, 0,
+  total(): number {
+    return this._items.reduce(
+      (acc, item) => acc + item.orderItemTotal(), 0,
     );
-
-    return this._total;
   }
 }
